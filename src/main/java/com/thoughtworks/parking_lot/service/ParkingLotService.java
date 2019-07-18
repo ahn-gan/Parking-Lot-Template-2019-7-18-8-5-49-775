6be880ef7b68983346a7fd071b01d42b23f1ba84
Akin <akin.gan@oocl.com>
2019-07-18 22:09:06 +0800

    fix: update the findParkingLots in ParkingLotService for pageable.

--- src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
+++ src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
@@ -31,7 +31,6 @@ public class ParkingLotService {
     }
 
     public List<ParkingLot> findParkingLots(int page, int pageSize) {
-        List<ParkingLot> parkingLots = findAll();
-        return (page == 0 || pageSize == 0 || pageSize > parkingLots.size()) ? parkingLots : parkingLotRepository.findAll(PageRequest.of(page -1, pageSize)).getContent();
+        return (page == 0 || pageSize == 0) ? findAll() : parkingLotRepository.findAll(PageRequest.of(page -1, pageSize)).getContent();
     }
 }
